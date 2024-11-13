a4a0e64ecbcfbb670619b501a27e3275b1ef976b
savostyuk <savostyuk@gmail.com>
2024-11-13 13:57:09 +0100

    Add base entites

---
+++ DiscountSystem.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DiscountSystem.Domain.Common
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }  
+        public Guid CreatedBy { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+        public Guid LastModifiedBy { get; set; }
+
+    }
+}
---
+++ DiscountSystem.Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace DiscountSystem.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
